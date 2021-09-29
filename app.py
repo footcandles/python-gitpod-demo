2696ad8498c50a5e0deeefcbb241d80006d7d2c0
Syed Naqiul Hasan Rizvi <hasannaqi9@gmail.com>
2021-09-29 18:13:44 +0000

    updated heroku file

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import os
 from flask import Flask
-
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"]= os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"]= os.environ.get("MONGO_URI")
+app.secret_key=os.environ.get("SECRET_KEY")
 
 @app.route("/")
 def hello():
@@ -16,4 +21,4 @@ if __name__ =="__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True
-            )
+            )      
