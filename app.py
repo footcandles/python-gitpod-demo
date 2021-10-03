b42c1368dbf7c2e506463d7a74542b4fce5a6d64
Syed Naqiul Hasan Rizvi <hasannaqi9@gmail.com>
2021-10-03 05:38:58 +0000

    connected to mongodb

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask
+from flask import (Flask, flash, render_template, redirect,
+    request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
@@ -12,9 +13,13 @@ app.config["MONGO_DBNAME"]= os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"]= os.environ.get("MONGO_URI")
 app.secret_key=os.environ.get("SECRET_KEY")
 
+mongo = PyMongo(app)
+
 @app.route("/")
-def hello():
-    return "Hello world--again"
+@app.route("/get_jobs")
+def getJobs():
+    jobs = mongo.db.jobs.find()
+    return render_template("jobs.html",jobs=jobs)
 
 
 if __name__ =="__main__":
---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault("MONGO_DBNAME","job_zone" )
+os.environ.setdefault("IP","0.0.0.0")
+os.environ.setdefault("PORT","5000")
+os.environ.setdefault("SECRET_KEY","1223dsdr3ds")
+os.environ.setdefault("MONGO_URI","mongodb+srv://naqi:nack@cluster0.o9n8m.azure.mongodb.net/job_zone?retryWrites=true&w=majority")
