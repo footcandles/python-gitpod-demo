d663c4294d883ef60b49130b66fc2a68a6bafd7f
Syed Naqiul Hasan Rizvi <hasannaqi9@gmail.com>
2021-10-03 07:02:59 +0000

    registeration ui

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import (Flask, flash, render_template, redirect,
     request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -17,9 +18,14 @@ mongo = PyMongo(app)
 
 @app.route("/")
 @app.route("/get_jobs")
-def getJobs():
+def get_jobs():
     jobs = mongo.db.jobs.find()
     return render_template("jobs.html",jobs=jobs)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 
 if __name__ =="__main__":
