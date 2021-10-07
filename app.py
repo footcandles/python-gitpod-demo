271d1d417b51b7f9404dc3b75306a3b3001a7fb9
Syed Naqiul Hasan Rizvi <hasannaqi9@gmail.com>
2021-10-07 18:19:36 +0000

    image upload

--- app.py
+++ app.py
@@ -1,26 +1,29 @@
 import os
-from flask import (Flask, flash, render_template, redirect,
-    request, session, url_for)
+from flask import (Flask, flash, render_template,
+ redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug import secure_filename
+
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"]= os.environ.get("MONGO_DBNAME")
-app.config["MONGO_URI"]= os.environ.get("MONGO_URI")
-app.secret_key=os.environ.get("SECRET_KEY")
+app.config["MONGO_DBNAME"]=os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"]=os.environ.get("MONGO_URI")
+app.secret_key =os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
+
 
 @app.route("/")
 @app.route("/get_jobs")
 def get_jobs():
     jobs = mongo.db.jobs.find()
-    return render_template("jobs.html",jobs=jobs)
+    return render_template("jobs.html",jobs = jobs)
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -29,30 +32,110 @@ def register():
         #check if username already exists in db
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
-
+        
         if existing_user:
-            flash('Username already exists')
+            flash("Username already exists")
             return redirect(url_for("register"))
-
-        register={
-            "username": request.form.get("username").lower(),
+        
+        register = {
+            "username": request.form.get("username"),
             "password": generate_password_hash(request.form.get("password")),
-            "email": request.form.get("email").lower(),
-            "firstname": request.form.get("firstname").lower(),
-            "lastname": request.form.get("lastname").lower(),
+            "firstname": request.form.get("firstname"),
+            "lastname": request.form.get("lastname"),
+            "email": request.form.get("email"),
             "jobseeker": "true" if request.form["role"] == "jobseeker" else "false",
             "employer": "true" if request.form["role"] == "employer" else "false"
         }
         mongo.db.users.insert_one(register)
-
-        #put the new user into 'session' cooking
-        session["user"]=request.form.get("username").lower()
-        flash("Registration successful")    
+        
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username")
+        flash("Registration successful")
+        return redirect(url_for("profile", username=session["user"]))
     return render_template("register.html")
 
 
-if __name__ =="__main__":
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        
+        if existing_user:
+            # ensure hashed password matches user imput
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username")
+                    return redirect(url_for("profile", username=session["user"]))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+        
+        else:
+            #username doesn't exists
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+@app.route("/get_employer")
+def get_employer():
+    #employer = mongo.db.employer.find()
+    return render_template("employer.html") 
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    if request.method == "POST":
+        profile = {
+            "username": session["user"],
+            "firstname": request.form.get("firstname"),
+            "lastname": request.form.get("lastname"),
+            "company": request.form.get("company"),
+            "designation": request.form.get("designation"),
+            "phone": request.form.get("phone"),
+            "address": request.form.get("address")
+        }
+        mongo.db.employer_profile.insert_one(profile)
+        profile = mongo.db.employer_profile.find_one(
+                    {"username": session["user"]})
+        return render_template(
+            "employer/profile.html", profile=profile)
+
+    username = mongo.db.users.find_one(
+            {"username": session["user"]})
+    session["role"] = "jobseeker" if username["jobseeker"] == 'true' else "employer"
+    # render profile page if session contains user's information
+    if session["user"]:
+        profile = mongo.db.employer_profile.find_one(
+                    {"username": session["user"]})
+        return render_template(
+            "employer/profile.html", profile=profile)
+    
+    return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    # remove user from session cookies
+    flash("You have been logged out")
+    session.pop("user")
+    session.pop("role")
+    #session.pop("profile")
+    return redirect(url_for("login"))
+
+
+@app.route('/upload', methods = ['GET', 'POST'])
+def upload():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      return redirect(url_for("profile", username=session["user"]))
+
+
+if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True
-            )      
+            )
