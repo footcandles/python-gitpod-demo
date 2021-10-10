98e92d23d901d78e3d240828703a98987ece2ff1
Syed Naqiul Hasan Rizvi <hasannaqi9@gmail.com>
2021-10-10 07:10:07 +0000

    job seeker registration

--- app.py
+++ app.py
@@ -20,10 +20,9 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
-@app.route("/get_jobs")
-def get_jobs():
-    jobs = mongo.db.jobs.find()
-    return render_template("jobs.html",jobs = jobs)
+@app.route("/home")
+def home():
+    return render_template("home.html")
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -31,27 +30,39 @@ def register():
     if request.method == "POST":
         #check if username already exists in db
         existing_user = mongo.db.users.find_one(
-            {"username": request.form.get("username")})
+            {"username": request.form.get("username").lower()})
         
         if existing_user:
             flash("Username already exists")
             return redirect(url_for("register"))
-        
+        jobSeekerRole = "true" if request.form["role"] == "jobseeker" else "false"
+        employerRole = "true" if request.form["role"] == "employer" else "false"
         register = {
             "username": request.form.get("username"),
             "password": generate_password_hash(request.form.get("password")),
             "firstname": request.form.get("firstname"),
             "lastname": request.form.get("lastname"),
             "email": request.form.get("email"),
-            "jobseeker": "true" if request.form["role"] == "jobseeker" else "false",
-            "employer": "true" if request.form["role"] == "employer" else "false"
+            "jobseeker":jobSeekerRole,
+            "employer": employerRole
         }
         mongo.db.users.insert_one(register)
-        
-        # put the new user into 'session' cookie
+        profile={
+            "username": request.form.get("username"),
+            "firstname": request.form.get("firstname"),
+            "lastname": request.form.get("lastname")
+        }
         session["user"] = request.form.get("username")
+        if employerRole == "true":
+            session["role"] = "employer"
+            mongo.db.employer_profile.insert_one(profile)
+            return redirect(url_for("profile", username=session["user"]))
+        else:
+            session["role"] = "jobseeker"
+            mongo.db.job_seeker_profile.insert_one(profile)
+            return redirect(url_for("jobSeekerProfile", username=session["user"]))
+        # put the new user into 'session' cookie
         flash("Registration successful")
-        return redirect(url_for("profile", username=session["user"]))
     return render_template("register.html")
 
 
@@ -65,13 +76,17 @@ def login():
             # ensure hashed password matches user imput
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
-                    session["user"] = request.form.get("username")
+                session["user"] = request.form.get("username").lower()
+                if existing_user["employer"] == "true":
+                    session["role"] = "employer"
                     return redirect(url_for("profile", username=session["user"]))
+                else:
+                    session["role"] = "jobseeker"
+                    return redirect(url_for("jobSeekerProfile", username=session["user"]))
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
                 return redirect(url_for("login"))
-        
         else:
             #username doesn't exists
             flash("Incorrect Username and/or Password")
@@ -79,14 +94,11 @@ def login():
 
     return render_template("login.html")
 
-@app.route("/get_employer")
-def get_employer():
-    #employer = mongo.db.employer.find()
-    return render_template("employer.html") 
-
 
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
+    user = mongo.db.users.find_one(
+            {"username": session["user"]})
     if request.method == "POST":
         f = request.files['file']
         if f:
@@ -99,6 +111,7 @@ def profile(username):
                 "designation": request.form.get("designation"),
                 "phone": request.form.get("phone"),
                 "address": request.form.get("address"),
+                "companydesc": request.form.get("companydesc"),
                 "imageurl": "images/"+f.filename
             }
         else:
@@ -110,26 +123,99 @@ def profile(username):
                 "designation": request.form.get("designation"),
                 "phone": request.form.get("phone"),
                 "address": request.form.get("address"),
+                "companydesc": request.form.get("companydesc"),
             }
         mongo.db.employer_profile.update_one({"username":session["user"]},{"$set":profile})
         # mongo.db.employer_profile.insert_one(profile)
         profile = mongo.db.employer_profile.find_one(
                     {"username": session["user"]})
+        profile["email"] = user["email"]
+        flash("Profile updated successfully")
         return render_template(
             "employer/profile.html", profile=profile)
 
-    user = mongo.db.users.find_one(
-            {"username": session["user"]})
     session["role"] = "jobseeker" if user["jobseeker"] == 'true' else "employer"
     # render profile page if session contains user's information
     if session["user"]:
         profile = mongo.db.employer_profile.find_one(
                     {"username": session["user"]})
-        profile["firstname"] = user["firstname"]
-        profile["lastname"] = user["lastname"]
         return render_template(
-            "employer/profile.html", profile = profile)
-    
+            "employer/profile.html", profile=profile)
+    return redirect(url_for("login"))
+
+
+@app.route("/jobSeekerProfile/<username>", methods=["GET", "POST"])
+def jobSeekerProfile(username):
+    user = mongo.db.users.find_one(
+            {"username": session["user"]})
+    if request.method == "POST":
+        imageFile = request.files['imageFile']
+        resumeDoc = request.files['resumeDoc']
+        if imageFile and resumeDoc:
+            imageFile.save(os.path.join("static/images/", secure_filename(imageFile.filename)))
+            resumeDoc.save(os.path.join("static/resume/", secure_filename(resumeDoc.filename)))
+            profile = {
+                "username": session["user"],
+                "firstname": request.form.get("firstname"),
+                "lastname": request.form.get("lastname"),
+                "profiledesc": request.form.get("profiledesc"),
+                "skills": request.form.get("skills"),
+                "phone": request.form.get("phone"),
+                "address": request.form.get("address"),
+                "imageurl": "images/"+imageFile.filename,
+                "resumeurl": "resume/"+resumeDoc.filename
+            }
+        elif imageFile:
+            imageFile.save(os.path.join("static/images/", secure_filename(imageFile.filename)))
+            profile = {
+                "username": session["user"],
+                "firstname": request.form.get("firstname"),
+                "lastname": request.form.get("lastname"),
+                "profiledesc": request.form.get("profiledesc"),
+                "skills": request.form.get("skills"),
+                "phone": request.form.get("phone"),
+                "address": request.form.get("address"),
+                "imageurl": "images/"+imageFile.filename
+            }
+        elif resumeDoc:
+            resumeDoc.save(os.path.join("static/resume/", secure_filename(resumeDoc.filename)))
+            profile = {
+                "username": session["user"],
+                "firstname": request.form.get("firstname"),
+                "lastname": request.form.get("lastname"),
+                "profiledesc": request.form.get("profiledesc"),
+                "skills": request.form.get("skills"),
+                "phone": request.form.get("phone"),
+                "address": request.form.get("address"),
+                "resumeurl": "resume/"+resumeDoc.filename
+            }
+        else:
+            profile = {
+                "username": session["user"],
+                "firstname": request.form.get("firstname"),
+                "lastname": request.form.get("lastname"),
+                "profiledesc": request.form.get("profiledesc"),
+                "skills": request.form.get("skills"),
+                "phone": request.form.get("phone"),
+                "address": request.form.get("address")
+            }
+        existing_profile = mongo.db.job_seeker_profile.find_one({"username":session["user"]})
+        if existing_profile:
+            mongo.db.job_seeker_profile.update_one({"username":session["user"]},{"$set":profile})
+        else:
+            mongo.db.job_seeker_profile.insert_one(profile)
+        profile = mongo.db.job_seeker_profile.find_one(
+                    {"username": session["user"]})
+        # profile["email"] = user["email"]
+        flash("Profile updated successfully")
+        return render_template(
+            "jobseeker/profile.html", profile=profile)
+    # render profile page if session contains user's information
+    if session["user"]:
+        profile = mongo.db.job_seeker_profile.find_one(
+                    {"username": session["user"]})
+        return render_template(
+            "jobseeker/profile.html", profile=profile)
     return redirect(url_for("login"))
 
 
@@ -155,18 +241,45 @@ def postJob():
                 "salary": request.form.get("salary")
             }
         mongo.db.jobs.insert_one(job)
+        flash("New Job created successfully")
         # mongo.db.employer_profile.insert_one(profile)
         savedJob = mongo.db.jobs.find_one(
                     {"username": session["user"]})
         return render_template(
-            "employer/postJob.html", savedJob = savedJob)
+            "employer/postJob.html", savedJob=savedJob)
     return render_template("employer/postJob.html")
 
 
 @app.route("/jobs", methods=["GET", "POST"])
 def jobs():
     jobs = mongo.db.jobs.find()
-    return render_template("employer/jobs.html", jobs = jobs)
+    return render_template("employer/jobs.html", jobs=jobs)
+
+
+@app.route("/ajax_update", methods=["POST","GET"])
+def ajax_update():
+    if request.method == 'POST':
+        job = {
+                "title": request.form['txtTitle'],
+                "description": request.form['txtDescription'],
+                "skills": request.form['txtSkills'],
+                "location": request.form['txtLocation'],
+                "salary": request.form['txtSalary']
+            }
+        mongo.db.jobs.update_one({"_id": ObjectId(request.form['id'])}, {"$set":job})
+        jobs = mongo.db.jobs.find()
+        return "job updated successfully"
+    jobs = mongo.db.jobs.find()
+    return render_template("employer/jobs.html", jobs=jobs)
+
+
+@app.route("/ajax_delete", methods=["POST", "GET"])
+def ajax_delete():
+    if request.method == 'POST':
+        mongo.db.jobs.delete_one({"_id": ObjectId(request.form['id'])})
+        return "Deleted Successfully"
+    jobs = mongo.db.jobs.find()
+    return render_template("employer/jobs.html", jobs=jobs)
 
 
 if __name__ == "__main__":
--- static/js/script.js
+++ static/js/script.js
@@ -1,26 +1,65 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav({edge:"right"});
-    $(".edit").click(function(){
-      $(this).attr("disabled","disabled")
-      $(this).parents("tr").find("td:not(:last-child)").each(function(i){
-        if(i=='0'){
-          var idname = 'txtTitle';
-        }
-        else if(i=='1'){
-          var idname = 'txtDescription';
-        }
-        else if(i=='2'){
-          var idname = 'txtSkills';
-        }
-        else if(i=='3'){
-          var idname = 'txtSalary';
-        }
-        else if(i=='4'){
-          var idname = 'txtLocation';
-        }
-        $(this).html("<input type='text' name='updaterec' id='"+idname+"' class='input-field' value='"+$(this).text()+"'> ")
-      })
-      $(this).parents('tr').find('.edit').toggle();
-    })
+$(document).ready(function () {
+  $('.sidenav').sidenav({
+    edge: "right"
   });
+  $(".update").hide();
+  $("tr td:nth-child(1)").hide()
+  $("#jobIdHeader").css("display", "none")
+  $(".edit").click(function () {
+    $(this).attr("disabled", "disabled")
+    $(this).parents("tr").find("td:not(:last-child)").each(function (i) {
+      if (i == '1') {
+        var idname = 'txtTitle';
+      } else if (i == '2') {
+        var idname = 'txtDescription';
+      } else if (i == '3') {
+        var idname = 'txtSkills';
+      } else if (i == '4') {
+        var idname = 'txtSalary';
+      } else if (i == '5') {
+        var idname = 'txtLocation';
+      }
+      $(this).html("<input type='text' name='updaterec' id='" + idname + "' class='input-field' value='" + $(this).text() + "'> ")
+    })
+    $(this).parents('tr').find('.update').show();
+    $(this).hide();
+    $(this).parents('tr').find('.delete').removeClass('delete').addClass("update");
+  })
+
+
+  $(".update").click(function () {
+    var id = $(this).parents('tr').find('td:nth-child(1)').find('input')[0].defaultValue;
+    var txtTitle = $("#txtTitle").val();
+    var txtDescription = $("#txtDescription").val();
+    var txtSkills = $("#txtSkills").val();
+    var txtSalary = $("#txtSalary").val();
+    var txtLocation = $("#txtLocation").val();
+    $.post("/ajax_update", {
+      id: id,
+      txtTitle: txtTitle,
+      txtDescription: txtDescription,
+      txtSkills: txtSkills,
+      txtSalary: txtSalary,
+      txtLocation: txtLocation
+    }, function (data) {
+      $("#displayMessage").html(data);
+      $("#displayMessage").show();
+      setTimeout(function(){
+        location.reload();
+      },1000)
       
+    });
+  });
+
+
+  $(document).on("click", ".delete", function(){
+    var id = $(this).parents('tr').find('td:nth-child(1)')[0].innerHTML
+    $(this).parents("tr").remove();
+    $.post("/ajax_delete", { id: id}, function(data) {
+        $("#displayMessage").html(data);
+        $("#displayMessage").show();
+    });
+});
+
+  
+});
